49ceabc39ef7fe37dfc4ed46df51744f68e3b23f
Lecouteux <jeanmary@gmail.com>
2019-10-05 20:07:45 +0200

    create new deploy env script

---
+++ master_rsync.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rsync -rv -e ssh --delete-after /var/www/html/classy_x_v2/build/ jm@192.168.1.170:/var/www/html/classy-x.com
+
+
--- rsync.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -rv -e ssh --delete-after /var/www/html/classy_x_v2/build/ jm@192.168.1.160:/var/www/html/classy-x.com
-
-
